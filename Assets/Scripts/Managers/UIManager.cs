49a3b78bf212356022592f251d5da13dda375e53
davidsoler <david.soler@udc.es>
2023-03-29 22:21:03 +0200

    Ahora está 100%

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -5,16 +5,8 @@ using UnityEngine.Events;
 
 public class UIManager : Singleton<UIManager>
 {
-    [SerializeField] PauseMenu _pauseMenu;
     [SerializeField] ScoreText _scoreText;
 
-    private void Start() {
-        GameManager.Instance.OnGameStateChanged.AddListener(HandleGameStateChanged);
-    }
-
-    private void HandleGameStateChanged(GameManager.GameState currentState, GameManager.GameState previousState) {
-        _pauseMenu.gameObject.SetActive(currentState == GameManager.GameState.PAUSED);
-    }
 
     public void UpdateScore(int score)
     {
